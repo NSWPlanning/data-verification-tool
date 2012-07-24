37917cb618e9f2e6061db46295954941626dfd87
Malcolm Locke <malc@wholemeal.co.nz>
2012-07-25 09:53:42 +1200

    Initial commit
    From `rails new dvt --old-style-hash -T --skip-bundle`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvt::Application.config.secret_token = 'e377fd1948048ff5a085b4c9025f611016d7c088a9e3fb38effbb818c75460af7f328ce86018624763499fe213da4455e9f1cee5c7c2b29a7ee03e086baff648'
