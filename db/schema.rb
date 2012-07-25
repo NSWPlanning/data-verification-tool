ddfbc683ad5127ee4a6c11bf06ca9eb335b211ee
Malcolm Locke <malc@wholemeal.co.nz>
2012-07-26 11:41:01 +1200

    Add migration for paper_trail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724223540) do
+ActiveRecord::Schema.define(:version => 20120725233950) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :null => false
@@ -21,5 +21,16 @@ ActiveRecord::Schema.define(:version => 20120724223540) do
     t.datetime "created_at",                          :null => false
     t.datetime "updated_at",                          :null => false
   end
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
