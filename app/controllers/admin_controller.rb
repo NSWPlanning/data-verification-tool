a9029f07071ea1b7d25208f30021209aa817d415
Malcolm Locke <malc@wholemeal.co.nz>
2012-07-26 10:47:45 +1200

    Add AdminController and AuthenticatedController
    Base controllers for protected controller classes

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < AuthenticatedController
+  before_filter :require_admin!
+
+  def require_admin!
+    render :nothing => true, :status => :forbidden unless current_user.admin?
+  end
+end
