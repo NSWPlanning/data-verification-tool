8d4df79deeeffe2ad460c59a2a4541e86da050c4
Malcolm Locke <malc@wholemeal.co.nz>
2012-07-30 15:31:25 +1200

    Add controller action for `users/admin`

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < AdminController
 
   include ResourceController
 
+  def admin
+    @title = 'Admin Users'
+    @users = resource_scope.with_roles(:admin)
+    render 'index'
+  end
+
+  protected
+  def resource_scope
+    User
+  end
+
 end
