5c416948465bb82036faf33954dc824f270598b6
Malcolm Locke <malc@wholemeal.co.nz>
2012-08-08 10:57:31 +1200

    Add LPI::Record#md5sum method

--- spec/lib/lpi/record_spec.rb
+++ spec/lib/lpi/record_spec.rb
@@ -11,4 +11,16 @@ describe LPI::Record do
   describe '#respond_to?' do
     pending
   end
+
+  describe '#md5sum' do
+
+    before do
+      row.stub(:to_csv => "abc123\n")
+    end
+
+    # echo 'abc123' | md5sum - | cut -f 1 -d ' '
+    # 2c6c8ab6ba8b9c98a1939450eb4089ed
+    its(:md5sum)  { should == '2c6c8ab6ba8b9c98a1939450eb4089ed' }
+  end
+
 end
