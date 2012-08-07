40b0743235fac9db9fc2cdc1b62df4fc670d1397
Malcolm Locke <malc@wholemeal.co.nz>
2012-08-08 10:57:31 +1200

    Add LPI::Record#md5sum method

--- lib/extra_libs/lpi/record.rb
+++ lib/extra_libs/lpi/record.rb
@@ -1,3 +1,4 @@
+require 'digest/md5'
 module LPI
   class Record
 
@@ -20,5 +21,11 @@ module LPI
       @row = row
     end
 
+    # Returns a checksum for this record, based on the CSV line content.
+    # Note that the checksum includes the trailing EOL character from the
+    # CSV record.
+    def md5sum
+      @md5sum ||= Digest::MD5.hexdigest(row.to_csv)
+    end
   end
 end
