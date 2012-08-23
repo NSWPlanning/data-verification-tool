4d46c5727710e51572b6182f23420a702d2e57f4
Malcolm Locke <malc@wholemeal.co.nz>
2012-08-24 11:11:21 +1200

    Modularize import logger behaviour

---
+++ app/models/import_log.rb
@@ -0,0 +1,56 @@
+# Common behaviour for import loggers
+require 'active_support/concern'
+
+module ImportLog
+
+  extend ActiveSupport::Concern
+
+  included do
+
+    belongs_to :user
+
+    attr_accessible :created, :error_count, :filename, :finished, :processed, :success,
+      :updated, :user_id, :finished_at
+
+    attr_accessor :importer
+    def complete!
+      update_attributes(
+        importer_attributes.merge(
+          :success => true, :finished => true, :finished_at => Time.now
+        )
+      )
+    end
+
+    def fail!
+      update_attributes(
+        importer_attributes.merge(
+          :success => false, :finished => true, :finished_at => Time.now
+        )
+      )
+    end
+
+    def started_at
+      created_at
+    end
+
+    protected
+    def importer_attributes
+      {
+        :processed => importer.processed, :created => importer.created,
+        :updated => importer.updated, :error_count => importer.error_count,
+      }
+    end
+
+  end
+
+  module ClassMethods
+    def start!(importer)
+      create!(
+        :filename => importer.filename, :user_id => importer.user.id
+      ).tap do |import_log|
+        import_log.importer = importer
+      end
+    end
+
+  end
+end
