4b6ae1c6aaf01a62c428f4f34b35693e61b94bfb
Malcolm Locke <malc@wholemeal.co.nz>
2012-08-30 10:42:47 +1200

    Add test data import integration spec

--- spec/integration/test_data_verifier_spec.rb
+++ spec/integration/test_data_verifier_spec.rb
@@ -19,7 +19,50 @@ describe 'test data verification' do
   it 'fails the specified validations correctly' do
     lpi_importer.import
     lga_importer.import
-    lga_importer.error_count.should == 17
+
+    lga_importer.should_not have_exception_on_line 2
+    lga_importer.should_not have_exception_on_line 3
+    lga_importer.should_not have_exception_on_line 4
+    lga_importer.should_not have_exception_on_line 5
+
+    # TODO LPI lookup scoped to LGA
+    #lga_importer.should have_exception_on_line 6
+    #lga_importer.should have_exception_on_line 7
+
+    # Invalid DP number
+    lga_importer.should have_exception_on_line 8
+    lga_importer.should have_exception_on_line 9
+
+    # TODO Duplicate DP number
+    #lga_importer.should have_exception_on_line 10
+    #lga_importer.should have_exception_on_line 11
+
+    # Missing postcode
+    lga_importer.should have_exception_on_line 12
+
+    # Missing suburb
+    lga_importer.should have_exception_on_line 13
+
+    # From street number is zero
+    lga_importer.should have_exception_on_line 14
+
+    lga_importer.should_not have_exception_on_line 15
+    lga_importer.should_not have_exception_on_line 16
+
+    # TODO inconsistent SP records
+    # lga_importer.should have_exception_on_line 17
+    # lga_importer.should have_exception_on_line 18
+    # lga_importer.should have_exception_on_line 19
+
+    # Council ID is missing
+    lga_importer.should have_exception_on_line 20
+
+    # Attributes blank
+    lga_importer.should have_exception_on_line 21
+
+    # TODO Only in LPI
+
+    pending "lga_importer.error_count.should == 17"
   end
 
 end
