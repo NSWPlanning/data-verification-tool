a50fbb2353f9c8753acbb72b069eaf001c560e19
Malcolm Locke <malc@wholemeal.co.nz>
2012-09-03 11:36:35 +1200

    Refactor RecordField definitions

--- lib/dvt/lga/record.rb
+++ lib/dvt/lga/record.rb
@@ -4,70 +4,64 @@ module DVT
 
       include PlanLabelInstanceMethods
 
-      def self.fields
-        @fields ||= [
-          RecordField.new('Date_of_update'),
-          RecordField.new('Council_ID'),
-          RecordField.new('If_partial_lot'),
-          RecordField.new('DP_lot_number'),
-          RecordField.new('DP_section_number'),
-          RecordField.new('DP_plan_number'),
-          RecordField.new('Ad_unit_no'),
-          RecordField.new('Ad_st_no_from'),
-          RecordField.new('Ad_st_no_to'),
-          RecordField.new('Ad_st_name'),
-          RecordField.new('Ad_st_type'),
-          RecordField.new('Ad_st_type_suffix'),
-          RecordField.new('Ad_postcode'),
-          RecordField.new('Ad_suburb'),
-          RecordField.new('Ad_LGA_name'),
-          RecordField.new('Land_area'),
-          RecordField.new('Frontage'),
-          RecordField.new('LEP_NSI_zone'),
-          RecordField.new('LEP_SI_zone'),
-          RecordField.new('If_critical_habitat'),
-          RecordField.new('If_wilderness'),
-          RecordField.new('If_heritage_item'),
-          RecordField.new('If_heritage_conservation_area'),
-          RecordField.new('If_heritage_conservation_area_draft'),
-          RecordField.new('If_coastal_water'),
-          RecordField.new('If_coastal_lake'),
-          RecordField.new('If_SEPP14_with_100m_buffer'),
-          RecordField.new('If_SEPP26_with_100m_buffer'),
-          RecordField.new('If_aquatic_reserve_with_100m_buffer'),
-          RecordField.new('If_wet_land_with_100m_buffer'),
-          RecordField.new('If_Aboriginal_significance'),
-          RecordField.new('If_biodiversity_significance'),
-          RecordField.new('If_land_reserved_national_park'),
-          RecordField.new('If_land_reserved_flora_fauna_geo'),
-          RecordField.new('If_land_reserved_public_purpose'),
-          RecordField.new('If_unsewered_land'),
-          RecordField.new('If_acid_sulfate_soil'),
-          RecordField.new('If_fire_prone_area'),
-          RecordField.new('If_flood_control_lot'),
-          RecordField.new('Ex_buffer_area'),
-          RecordField.new('Ex_coastal_erosion_hazard'),
-          RecordField.new('Ex_ecological_sensitive_area'),
-          RecordField.new('Ex_protected_area'),
-          RecordField.new('If_foreshore_area'),
-          RecordField.new('Ex_environmentally_sensitive_land'),
-          RecordField.new('If_ANEF25'),
-          # transaction is a protected word in ActiveRecord
-          RecordField.new('Transaction', :aliases => ['transaction_type']),
-          RecordField.new('If_Western_Sydney_parkland'),
-          RecordField.new('If_river_front'),
-          RecordField.new('If_land_biobanking'),
-          RecordField.new('If_Sydney_water_special_area'),
-          RecordField.new('If_SEPP_Alpine_Resorts'),
-          RecordField.new('If_Siding_Springs_18km_buffer'),
-          RecordField.new('Acid_sulfate_soil_class'),
-          RecordField.new('If_mine_subsidence'),
-          RecordField.new('If_local_heritage_item'),
-          RecordField.new('If_Orana_REP')
-        ]
-      end
-
-      has_header_fields *header_fields
+      has_field 'Date_of_update'
+      has_field 'Council_ID'
+      has_field 'If_partial_lot'
+      has_field 'DP_lot_number'
+      has_field 'DP_section_number'
+      has_field 'DP_plan_number'
+      has_field 'Ad_unit_no'
+      has_field 'Ad_st_no_from'
+      has_field 'Ad_st_no_to'
+      has_field 'Ad_st_name'
+      has_field 'Ad_st_type'
+      has_field 'Ad_st_type_suffix'
+      has_field 'Ad_postcode'
+      has_field 'Ad_suburb'
+      has_field 'Ad_LGA_name'
+      has_field 'Land_area'
+      has_field 'Frontage'
+      has_field 'LEP_NSI_zone'
+      has_field 'LEP_SI_zone'
+      has_field 'If_critical_habitat'
+      has_field 'If_wilderness'
+      has_field 'If_heritage_item'
+      has_field 'If_heritage_conservation_area'
+      has_field 'If_heritage_conservation_area_draft'
+      has_field 'If_coastal_water'
+      has_field 'If_coastal_lake'
+      has_field 'If_SEPP14_with_100m_buffer'
+      has_field 'If_SEPP26_with_100m_buffer'
+      has_field 'If_aquatic_reserve_with_100m_buffer'
+      has_field 'If_wet_land_with_100m_buffer'
+      has_field 'If_Aboriginal_significance'
+      has_field 'If_biodiversity_significance'
+      has_field 'If_land_reserved_national_park'
+      has_field 'If_land_reserved_flora_fauna_geo'
+      has_field 'If_land_reserved_public_purpose'
+      has_field 'If_unsewered_land'
+      has_field 'If_acid_sulfate_soil'
+      has_field 'If_fire_prone_area'
+      has_field 'If_flood_control_lot'
+      has_field 'Ex_buffer_area'
+      has_field 'Ex_coastal_erosion_hazard'
+      has_field 'Ex_ecological_sensitive_area'
+      has_field 'Ex_protected_area'
+      has_field 'If_foreshore_area'
+      has_field 'Ex_environmentally_sensitive_land'
+      has_field 'If_ANEF25'
+      # transaction is a protected word in ActiveRecord
+      has_field 'Transaction', :aliases => ['transaction_type']
+      has_field 'If_Western_Sydney_parkland'
+      has_field 'If_river_front'
+      has_field 'If_land_biobanking'
+      has_field 'If_Sydney_water_special_area'
+      has_field 'If_SEPP_Alpine_Resorts'
+      has_field 'If_Siding_Springs_18km_buffer'
+      has_field 'Acid_sulfate_soil_class'
+      has_field 'If_mine_subsidence'
+      has_field 'If_local_heritage_item'
+      has_field 'If_Orana_REP'
 
       # Extra attributes that are not included in the CSV data, but are relevant
       # to the records
--- lib/dvt/lpi/record.rb
+++ lib/dvt/lpi/record.rb
@@ -3,23 +3,17 @@ module DVT
   module LPI
     class Record < BaseRecord
 
-      def self.fields
-        @fields ||= [
-          RecordField.new('CADID',          :aliases => [:cadastre_id]),
-          RecordField.new('LOTNUMBER',      :aliases => [:lot_number]),
-          RecordField.new('SECTIONNUMBER',  :aliases => [:section_number]),
-          RecordField.new('PLANLABEL',      :aliases => [:plan_label]),
-          # NOTE - This is called title_reference in the DB
-          RecordField.new('STD_DP_LOT_ID',  :aliases => [:title_reference]),
-          RecordField.new('STARTDATE',      :aliases => [:start_date]),
-          RecordField.new('ENDDATE',        :aliases => [:end_date]),
-          RecordField.new('MODIFIEDDATE',   :aliases => [:modified_date]),
-          RecordField.new('LASTUPDATE',     :aliases => [:last_update]),
-          RecordField.new('LGANAME',        :aliases => [:lga_name])
-        ]
-      end
-
-      has_header_fields *header_fields
+      has_field 'CADID',          :aliases => [:cadastre_id]
+      has_field 'LOTNUMBER',      :aliases => [:lot_number]
+      has_field 'SECTIONNUMBER',  :aliases => [:section_number]
+      has_field 'PLANLABEL',      :aliases => [:plan_label]
+      # NOTE - This is called title_reference in the DB
+      has_field 'STD_DP_LOT_ID',  :aliases => [:title_reference]
+      has_field 'STARTDATE',      :aliases => [:start_date]
+      has_field 'ENDDATE',        :aliases => [:end_date]
+      has_field 'MODIFIEDDATE',   :aliases => [:modified_date]
+      has_field 'LASTUPDATE',     :aliases => [:last_update]
+      has_field 'LGANAME',        :aliases => [:lga_name]
 
       # Extra attributes that are not included in the CSV data, but are relevant
       # to the records
