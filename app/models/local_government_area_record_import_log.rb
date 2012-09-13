9e03bdf1d54a658c24f6cb1726e4f3e0c67ba7c6
Malcolm Locke <malc@wholemeal.co.nz>
2012-09-13 16:07:49 +1200

    Ensure classes for serialization are loaded

--- app/models/local_government_area_record_import_log.rb
+++ app/models/local_government_area_record_import_log.rb
@@ -1,3 +1,5 @@
+require 'data_quality'
+require 'council_file_statistics'
 class LocalGovernmentAreaRecordImportLog < ActiveRecord::Base
 
   belongs_to :local_government_area
@@ -5,6 +7,8 @@ class LocalGovernmentAreaRecordImportLog < ActiveRecord::Base
   attr_accessible :local_government_area, :local_government_area_id,
     :data_quality, :council_file_statistics
 
+  # Make sure each of these has a corresponding require line in this
+  # files header.
   serialize :data_quality
   serialize :council_file_statistics
 
