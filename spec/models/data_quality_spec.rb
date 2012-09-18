31a6163936b0c9a3958c0948dcb07c67255ca837
Malcolm Locke <malc@wholemeal.co.nz>
2012-09-18 16:58:18 +1200

    Fix DataQuality percentages

--- spec/models/data_quality_spec.rb
+++ spec/models/data_quality_spec.rb
@@ -2,23 +2,13 @@ require 'lib_spec_helper'
 require_relative '../../app/models/import_statistics_set'
 require_relative '../../app/models/data_quality'
 
-RSpec::Matchers.define :have_percentage_method_for do |collection|
-  match do |actual|
-    count_method = "#{collection}_count"
-    percentage_method = "#{collection}_percentage" 
-    actual.stub(count_method => 50, :lpi_count => 100)
-    actual.send(percentage_method) == 50.0
-  end
-end
-
 describe DataQuality do
 
   let(:attributes)  {
     {
-      :in_council_and_lpi => 50,
-      :only_in_lpi        => 25,
-      :only_in_council    => 75,
-      :total              => 100
+      :in_council_and_lpi => 5,
+      :only_in_lpi        => 3,
+      :only_in_council    => 2
     }
   }
 
@@ -27,7 +17,7 @@ describe DataQuality do
   it_should_behave_like 'an import statistics set'
 
   its(:in_council_and_lpi_percentage) { should == 50.0 }
-  its(:only_in_lpi_percentage)        { should == 25.0 }
-  its(:only_in_council_percentage)    { should == 75.0 }
+  its(:only_in_lpi_percentage)        { should == 30.0 }
+  its(:only_in_council_percentage)    { should == 20.0 }
 
 end
