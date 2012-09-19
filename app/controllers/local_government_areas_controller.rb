05d149784f17c22b3b4d336db024cbfc53f8d49b
Malcolm Locke <malc@wholemeal.co.nz>
2012-09-19 15:49:22 +1200

    Basic HTTP auth for LGA uploads
    Aliases the LocalGovernmentAreasController#uploads action to
    LocalGovernmentAreasController#import to allow a different filter chain
    to be applied.

--- app/controllers/local_government_areas_controller.rb
+++ app/controllers/local_government_areas_controller.rb
@@ -4,8 +4,14 @@ class LocalGovernmentAreasController < AdminController
   # Access is still controlled by the find_model method, which won't allow
   # users who don't have access to this LGA.
   skip_before_filter :require_admin!, :only => [
-    :index, :show, :uploads, :detail, :error_records
+    :index, :show, :uploads, :detail, :error_records, :import
   ]
+
+  # Ensure that authentication goes through HTTP basic auth for the import
+  # action
+  skip_before_filter :verify_authenticity_token, :only => [:import]
+  skip_before_filter :require_login, :only => [:import]
+  before_filter :require_http_auth, :only => [:import]
 
   include ResourceController
 
@@ -18,8 +24,20 @@ class LocalGovernmentAreasController < AdminController
       :notice => 'Your data file will be processed shortly.'
   end
 
+  # The /import action is exactly the same as the /uploads action, except that
+  # it passes through a different set of before filters.
+  alias :import :uploads
+
   def error_records
     @local_government_area = find_model(params[:id])
     @title = "Error records for #{@local_government_area.name}"
+  end
+
+  protected
+  def require_http_auth
+    authenticate_with_http_basic do |username, password|
+      current_user = login(username, password)
+    end
+    render :nothing => true, :status => :forbidden unless current_user
   end
 end
