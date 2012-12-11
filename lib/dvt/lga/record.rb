543c6b2687c7fb01447c8290a81c150c41d0e776
Dave O'Flynn <dave.oflynn@sundered.org>
2012-12-11 14:29:20 +1100

    Making land-based attributes optional on a per-row basis.

--- lib/dvt/lga/record.rb
+++ lib/dvt/lga/record.rb
@@ -23,47 +23,47 @@ module DVT
       has_field 'Frontage'
       has_field 'LEP_NSI_zone'
       has_field 'LEP_SI_zone'
-# TODO: Metadata: Read optional fields from LGA-specific metadata form.
+# TODO: Metadata: Read required fields from LGA-specific metadata form.
 #       All fields that are required must be present.
-      has_field 'If_critical_habitat', required: true
-      has_field 'If_wilderness', required: true
-      has_field 'If_heritage_item'
-      has_field 'If_heritage_conservation_area'
-      has_field 'If_heritage_conservation_area_draft'
-      has_field 'If_coastal_water'
-      has_field 'If_coastal_lake'
-      has_field 'If_SEPP14_with_100m_buffer'
-#      has_field 'If_SEPP26_with_100m_buffer'
-#      has_field 'If_aquatic_reserve_with_100m_buffer'
-#      has_field 'If_wet_land_with_100m_buffer'
-      has_field 'If_Aboriginal_significance'
-      has_field 'If_biodiversity_significance'
-      has_field 'If_land_reserved_national_park'
-#      has_field 'If_land_reserved_flora_fauna_geo'
-      has_field 'If_land_reserved_public_purpose'
-#      has_field 'If_unsewered_land'
-#      has_field 'If_acid_sulfate_soil'
-      has_field 'If_fire_prone_area'
-      has_field 'If_flood_control_lot'
-#      has_field 'Ex_buffer_area'
-#      has_field 'Ex_coastal_erosion_hazard'
-#      has_field 'Ex_ecological_sensitive_area'
-#      has_field 'Ex_protected_area'
-#      has_field 'If_foreshore_area'
-#      has_field 'Ex_environmentally_sensitive_land'
-#      has_field 'If_ANEF25'
+      has_field 'If_critical_habitat',                  required: false
+      has_field 'If_wilderness',                        required: false
+      has_field 'If_heritage_item',                     required: false
+      has_field 'If_heritage_conservation_area',        required: false
+      has_field 'If_heritage_conservation_area_draft',  required: false
+      has_field 'If_coastal_water',                     required: false
+      has_field 'If_coastal_lake',                      required: false
+      has_field 'If_SEPP14_with_100m_buffer',           required: false
+      has_field 'If_SEPP26_with_100m_buffer',           required: false
+      has_field 'If_aquatic_reserve_with_100m_buffer',  required: false
+      has_field 'If_wet_land_with_100m_buffer',         required: false
+      has_field 'If_Aboriginal_significance',           required: false
+      has_field 'If_biodiversity_significance',         required: false
+      has_field 'If_land_reserved_national_park',       required: false
+      has_field 'If_land_reserved_flora_fauna_geo',     required: false
+      has_field 'If_land_reserved_public_purpose',      required: false
+      has_field 'If_unsewered_land',                    required: false
+      has_field 'If_acid_sulfate_soil',                 required: false
+      has_field 'If_fire_prone_area',                   required: false
+      has_field 'If_flood_control_lot',                 required: false
+      has_field 'Ex_buffer_area',                       required: false
+      has_field 'Ex_coastal_erosion_hazard',            required: false
+      has_field 'Ex_ecological_sensitive_area',         required: false
+      has_field 'Ex_protected_area',                    required: false
+      has_field 'If_foreshore_area',                    required: false
+      has_field 'Ex_environmentally_sensitive_land',    required: false
+      has_field 'If_ANEF25',                            required: false
       # transaction is a protected word in ActiveRecord
-#      has_field 'Transaction', :aliases => ['transaction_type']
-#      has_field 'If_Western_Sydney_parkland'
-      has_field 'If_river_front'
-#      has_field 'If_land_biobanking'
-#      has_field 'If_Sydney_water_special_area'
-#      has_field 'If_SEPP_Alpine_Resorts'
-#      has_field 'If_Siding_Springs_18km_buffer'
-#      has_field 'Acid_sulfate_soil_class'
-      has_field 'If_mine_subsidence'
-      has_field 'If_local_heritage_item'
-#      has_field 'If_Orana_REP'
+      has_field 'Transaction', :aliases => ['transaction_type'], required: false
+      has_field 'If_Western_Sydney_parkland',           required: false
+      has_field 'If_river_front',                       required: false
+      has_field 'If_land_biobanking',                   required: false
+      has_field 'If_Sydney_water_special_area',         required: false
+      has_field 'If_SEPP_Alpine_Resorts',               required: false
+      has_field 'If_Siding_Springs_18km_buffer',        required: false
+      has_field 'Acid_sulfate_soil_class',              required: false
+      has_field 'If_mine_subsidence',                   required: false
+      has_field 'If_local_heritage_item',               required: false
+      has_field 'If_Orana_REP',                         required: false
 
       # Extra attributes that are not included in the CSV data, but are relevant
       # to the records
