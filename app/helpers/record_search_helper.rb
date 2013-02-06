8ae516e4b6658fd4be90a7aa83e6d20d89561632
jgalilee <jgalilee@gmail.com>
2013-02-06 15:58:39 +1100

    Fix nil search query for LPI and LGA records

--- app/helpers/record_search_helper.rb
+++ app/helpers/record_search_helper.rb
@@ -7,9 +7,12 @@ module RecordSearchHelper
   module ClassMethods
 
     def search(filter, conditions={}, *fields)
-      filter_conditions = fields.zip(filter.split('/').reverse).reject { |k, v|
-        v.nil? || v.strip.empty?
-      }
+      filter_conditions = []
+      if !filter.blank?
+        filter_conditions = fields.zip(filter.split('/').reverse).reject { |k, v|
+          v.nil? || v.strip.empty?
+        }
+      end
       where(Hash[filter_conditions].merge(conditions))
     end
 
