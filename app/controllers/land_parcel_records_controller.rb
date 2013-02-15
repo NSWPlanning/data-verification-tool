9e5626c4f1b228a0de92b41d423de318fcc6bb60
jgalilee <jgalilee@gmail.com>
2013-02-15 16:28:23 +1100

    Remove extra land parcels breadcrumb

--- app/controllers/land_parcel_records_controller.rb
+++ app/controllers/land_parcel_records_controller.rb
@@ -2,18 +2,10 @@ class LandParcelRecordsController < ApplicationController
 
   respond_to :html
 
-  before_filter :add_landparcel_breadcrumb
-
   def show
     @land_parcel_record = LandParcelRecord.new(params[:id])
 
     add_breadcrumb @land_parcel_record.title_reference
-  end
-
-  protected
-
-  def add_landparcel_breadcrumb
-    add_breadcrumb "Land Parcels"
   end
 
 end
