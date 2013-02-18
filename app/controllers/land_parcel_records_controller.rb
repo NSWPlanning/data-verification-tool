eb281ec17f239aa945276d1cc6d17b548435765e
jgalilee <jgalilee@gmail.com>
2013-02-18 12:35:18 +1100

    Add authenticated access to land parcel records

--- app/controllers/land_parcel_records_controller.rb
+++ app/controllers/land_parcel_records_controller.rb
@@ -1,11 +1,34 @@
-class LandParcelRecordsController < ApplicationController
+class LandParcelRecordsController < AuthenticatedController
 
   respond_to :html
 
-  def show
-    @land_parcel_record = LandParcelRecord.new(params[:id])
+  before_filter :find_land_parcel, :scope => :show
+  before_filter :authenticate_land_parcel, :scope => :show
 
+  def show
     add_breadcrumb @land_parcel_record.title_reference
+  end
+
+  protected
+
+  def find_land_parcel
+    begin
+      @land_parcel_record = LandParcelRecord.new(params[:id])
+    rescue LandParcelRecord::RecordNotFound => e
+      raise ActiveRecord::RecordNotFound.new
+    end
+  end
+
+  def authenticate_land_parcel
+    unless current_user.admin?
+      land_parcel_lgas = @land_parcel_record.local_government_areas
+      user_lgas = current_user.local_government_areas
+      intersection = (land_parcel_lgas & user_lgas)
+      if intersection.blank? && !current_user.admin?
+        flash[:alert] = "You do not have access to this land parcel"
+        redirect_to :root
+      end
+    end
   end
 
 end
--- spec/controllers/land_parcel_records_controller_spec.rb
+++ spec/controllers/land_parcel_records_controller_spec.rb
@@ -2,23 +2,73 @@ require 'spec_helper'
 
 describe LandParcelRecordsController do
 
+  let(:admin) { FactoryGirl.create(:admin_user) }
+
   let!(:lpi_sp) {
     FactoryGirl.create :land_and_property_information_record,
       :title_reference => "//SP123"
   }
 
+  let!(:lga) {
+    FactoryGirl.create :local_government_area
+  }
+
+  let!(:lgar) {
+    FactoryGirl.create :local_government_area_record,
+      :dp_lot_number => "1",
+      :dp_plan_number => "DP123",
+      :local_government_area => lga
+  }
+
+  let(:normal_user) {
+    user = FactoryGirl.build(:user)
+    user.local_government_areas.push lga
+    user.save!
+    user
+  }
+
   describe "#show" do
 
-    it "should be a success" do
-      get :show, :id => "//SP123"
+    context "user is an admin" do
+      before do
+        login_user admin
+      end
 
-      response.should be_success
+      it "should be a success" do
+        get :show, :id => "//SP123"
+
+        response.should be_success
+      end
+
+      it "should render a 404 error for an invalid title reference" do
+        expect {
+          get :show, :id => "//SP321"
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
     end
 
-    it "should raise an error for an invalid id" do
-      expect {
-        get :show, :id => "//SP321"
-      }.to raise_error LandParcelRecord::RecordNotFound
+    context "user is not an admin" do
+      before do
+        login_user normal_user
+      end
+
+      it "should redirect the users without access to the root page" do
+        get :show, :id => "//SP123"
+
+        response.should redirect_to root_path
+      end
+
+      it "should be a success if the user is part of the lga" do
+        get :show, :id => "1//DP123"
+
+        response.should be_success
+      end
+
+      it "should render a 404 error for an invalid title reference" do
+        expect {
+          get :show, :id => "//SP321"
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
     end
 
   end
