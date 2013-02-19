e9e5dcdabb4e6983d78f064a9b59b91f50c50862
jgalilee <jgalilee@gmail.com>
2013-02-19 15:17:34 +1100

    Add search functionality to LPI records

--- app/models/land_and_property_information_record.rb
+++ app/models/land_and_property_information_record.rb
@@ -1,13 +1,30 @@
 class LandAndPropertyInformationRecord < ActiveRecord::Base
-  attr_accessible :cadastre_id, :end_date, :last_update, :lga_name,
-    :lot_number, :modified_date, :plan_label, :section_number,
-    :start_date, :title_reference, :md5sum, :local_government_area_id, :retired
-  validate :cadastre_id, :scope => :local_government_area_id,
+
+  include RecordSearchHelper
+
+  attr_accessible :cadastre_id,
+    :end_date,
+    :last_update,
+    :lga_name,
+    :lot_number,
+    :modified_date,
+    :plan_label,
+    :section_number,
+    :start_date,
+    :title_reference,
+    :md5sum,
+    :local_government_area_id,
+    :retired
+
+  validate :cadastre_id,
+    :scope => :local_government_area_id,
     :uniqueness => true
+
   validates :cadastre_id, :lga_name, :title_reference, :md5sum,
     :presence => true
 
   scope :retired, where(:retired => true)
+
   scope :active, where(:retired => false)
 
   def retire!
@@ -18,4 +35,9 @@ class LandAndPropertyInformationRecord < ActiveRecord::Base
   def common_property?
     title_reference.starts_with?("//SP")
   end
+
+  def self.search(filter, conditions = {})
+    super(filter, conditions, :plan_label, :section_number, :lot_number)
+  end
+
 end
--- spec/models/land_and_property_information_record_spec.rb
+++ spec/models/land_and_property_information_record_spec.rb
@@ -27,4 +27,78 @@ describe LandAndPropertyInformationRecord do
 
   end
 
+  describe "::search" do
+
+    let!(:lpir_1) {
+      FactoryGirl.create :land_and_property_information_record
+    }
+
+    let!(:lpir_2) {
+      FactoryGirl.create :land_and_property_information_record,
+        :plan_label => "DP666",
+        :lot_number => "1",
+        :section_number => "2"
+    }
+
+    let!(:lpir_3) {
+      FactoryGirl.create :land_and_property_information_record,
+        :plan_label => "DP666",
+        :lot_number => "1",
+        :section_number => "1"
+    }
+
+    let!(:lpir_4) {
+      FactoryGirl.create :land_and_property_information_record,
+        :plan_label => "DP666",
+        :lot_number => "2",
+        :section_number => "4"
+    }
+
+    context "only one item with the specified plan label" do
+      it "raises an error" do
+        expect {
+          LandAndPropertyInformationRecord.search("")
+        }.to raise_error
+      end
+
+      it "returns all matches for a plan label" do
+        label = lpir_1.plan_label
+        result = LandAndPropertyInformationRecord.search("#{label}")
+        result.first.should eq lpir_1
+      end
+
+      it "accepts additional search arguments" do
+        result = LandAndPropertyInformationRecord.search("DP666", :lga_name => "BOGAN SHIRE")
+        result.count.should eq 3
+      end
+    end
+
+    context "multiple items with the specified plan label" do
+      it "returns all of the matches for a specified plan label" do
+        label = lpir_2.plan_label
+        query = "#{label}"
+        result = LandAndPropertyInformationRecord.search(query)
+        result.count.should eq 3
+      end
+
+      it "narrows the search down for lot number" do
+        label = lpir_2.plan_label
+        lot_number = lpir_2.lot_number
+        query = "#{lot_number}//#{label}"
+        result = LandAndPropertyInformationRecord.search(query)
+        result.count.should eq 2
+      end
+
+      it "narrows the search down for lot number and section number" do
+        label = lpir_2.plan_label
+        lot_number = lpir_2.lot_number
+        section_number = lpir_2.section_number
+        query = "#{lot_number}/#{section_number}/#{label}"
+        result = LandAndPropertyInformationRecord.search(query)
+        result.count.should eq 1
+      end
+    end
+
+  end
+
 end
