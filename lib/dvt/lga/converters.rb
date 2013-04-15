69744d77527b53a66dec046bc74d77ed28397045
Dave O'Flynn <dave.oflynn@sundered.org>
2013-04-15 14:30:59 +1000

    Fix for overeager internal whitespace stripping. Ooops!

--- lib/dvt/lga/converters.rb
+++ lib/dvt/lga/converters.rb
@@ -7,7 +7,7 @@ module DVT
 
       # Strip internal space from plan label. That is, turn "DP 1234" into "DP1234"
       DP_PLAN_LABEL = lambda do |field, field_info|
-        return field unless field_info.header.casecmp('DP_plan_number')
+        return field unless field_info.header.casecmp('DP_plan_number') == 0
         field.gsub(" ", "")
       end
     end
