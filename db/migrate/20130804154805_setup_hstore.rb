a61a320df03bffa6e13d57eaf8274c5669c889f4
Dave O'Flynn <dave.oflynn@sundered.org>
2013-08-19 08:34:12 +0200

    Remove hstore create - has to be manually run.

--- db/migrate/20130804154805_setup_hstore.rb
+++ db/migrate/20130804154805_setup_hstore.rb
@@ -1,9 +1,11 @@
 class SetupHstore < ActiveRecord::Migration
   def self.up
-    execute "CREATE EXTENSION IF NOT EXISTS hstore"
+# Production boxes don't have the rights to do this    
+#    execute "CREATE EXTENSION IF NOT EXISTS hstore"
   end
 
   def self.down
-    execute "DROP EXTENSION IF EXISTS hstore"
+# Production boxes don't have the rights to do this        
+#    execute "DROP EXTENSION IF EXISTS hstore"
   end
 end
